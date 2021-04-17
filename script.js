146dae9ea01a0e8b5d22e109ab89756070cdb85f
fahzywan <fahzywan@gmail.com>
2021-04-18 06:10:18 +0700

    Add footer

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ $(document).ready(function() {
         } else {
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script 
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
     });
     
     // toggle menu / navbar script
@@ -13,4 +24,13 @@ $(document).ready(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    // typing animation script
+
+    var typed = new Typed (".typing", {
+        strings: ["UI and UX Designer","Web Developer", "Graphic Designer"],
+        typespeed: 100,
+        backspeed: 10,
+        loop: true
+    })
 });
